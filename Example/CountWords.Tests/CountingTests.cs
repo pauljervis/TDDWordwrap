1fe69f409310df3896d330c22a98e9059175704f
DavidBetteridge <davidabetteridge@gmail.com>
2017-04-10 09:21:20 +0100

    Example TDD

---
+++ Example/CountWords.Tests/CountingTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Example.CountWords;
+
+namespace CountWords.Tests
+{
+    [TestClass]
+    public class CountingTests
+    {
+        [TestMethod]
+        public void WhenGivenAnEmptyStringReturnZero()
+        {
+            var service = new Counter();
+            var expectedResult = 0;
+            var input = "";
+
+            var actualResult = service.CountWords(input);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+        //[TestMethod]
+        //public void WhenGivenASingleWordReturnOne()
+        //{
+        //    var service = new Counter();
+        //    var expectedResult = 1;
+        //    var input = "hello";
+
+        //    var actualResult = service.CountWords(input);
+        //    Assert.AreEqual(expectedResult, actualResult);
+        //}
+
+        //// Remove usings
+
+        //[TestMethod]
+        //public void WhenGivenTwoWordsReturnTwo()
+        //{
+        //    var service = new Counter();
+        //    var expectedResult = 2;
+        //    var input = "hello world";
+
+        //    var actualResult = service.CountWords(input);
+        //    Assert.AreEqual(expectedResult, actualResult);
+        //}
+
+        ////Refactor - count spaces
+
+        //[TestMethod]
+        //public void WhenGivenThreeWordsWithExtraSpacesStillReturnThree()
+        //{
+        //    var service = new Counter();
+        //    var expectedResult = 3;
+        //    var input = "hello from  York";
+
+        //    var actualResult = service.CountWords(input);
+        //    Assert.AreEqual(expectedResult, actualResult);
+        //}
+
+    }
+}
---
+++ Example/CountWords.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CountWords.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CountWords.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b379f14a-3eed-4a88-9533-07461aecc0cf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
