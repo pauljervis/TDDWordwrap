84e84b43534f18c560d901d515d73cea04691a62
Paul Jervis <jervis@12os.co.uk>
2017-04-12 19:46:46 +0100

    add project and Unit files

---
+++ src/com/yorkdevelopers/tdd/Wrapper.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Paul Jervis on 12/04/2017.
+ */
+package com.yorkdevelopers.tdd;
+
+public class Wrapper {
+}
---
+++ src/com/yorkdevelopers/tdd/WrapperTest.java
@@ -0,0 +1,10 @@
+package com.yorkdevelopers.tdd;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Paul Jervis on 12/04/2017.
+ */
+class WrapperTest {
+
+}
